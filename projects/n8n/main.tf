8864ee31065030d3fbf6e80673099be48f1e388e
Jesus David Posada Escobar <posesco@gmail.com>
2025-12-13 07:44:51 +0100

    Add data resources into main.tf

--- projects/n8n/main.tf
+++ projects/n8n/main.tf
@@ -7,3 +7,38 @@ module "common_tags" {
 locals {
   common_tags = module.common_tags.tags
 }
+
+data "terraform_remote_state" "networking" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-bucket-unique-posesco"
+    key    = "foundation/networking/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-bucket-unique-posesco"
+    key    = "foundation/iam/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "os" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
