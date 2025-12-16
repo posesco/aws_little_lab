c29a871228e62adf9849ac87239eb446ec15f790
Jesus David Posada Escobar <posesco@gmail.com>
2025-12-16 17:51:57 +0100

    Clean security.tf, delete comments

--- projects/n8n/security.tf
+++ projects/n8n/security.tf
@@ -28,54 +28,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv6" {
   to_port           = 22
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_no_tls_ipv4" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 81
-#   ip_protocol       = "tcp"
-#   to_port           = 81
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_no_tls_ipv6" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv6         = "::/0"
-#   from_port         = 81
-#   ip_protocol       = "tcp"
-#   to_port           = 81
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv6         = "::/0"
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_n8n_ipv4" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 5678
-#   ip_protocol       = "tcp"
-#   to_port           = 5678
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_n8n_ipv6" {
-#   security_group_id = aws_security_group.lab_sg.id
-#   cidr_ipv6         = "::/0"
-#   from_port         = 5678
-#   ip_protocol       = "tcp"
-#   to_port           = 5678
-# }
-
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.lab_sg.id
   cidr_ipv4         = "0.0.0.0/0"
