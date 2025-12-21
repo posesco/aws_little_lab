d4611820d5affd70a25cac765cb5259796b6972c
Jesus David Posada Escobar <posesco@gmail.com>
2025-12-21 21:08:52 +0100

    Implementation of workspaces and resource diagram in billing module

--- foundation/billing/main.tf
+++ foundation/billing/main.tf
@@ -1,11 +1,17 @@
+locals {
+  env          = terraform.workspace
+  budget_limit = lookup(var.budget_limits, local.env, var.budget_limits["dev"])
+}
+
 module "common_tags" {
   source  = "../../modules/common-tags"
-  env     = var.env
+  env     = local.env
   project = "foundation"
   additional_tags = {
     Component = "billing-alerts"
   }
 }
+
 locals {
   common_tags = module.common_tags.tags
 }
