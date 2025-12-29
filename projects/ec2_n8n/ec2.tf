06efb42c3cdcbd191b73236b642526481c70b7c7
Jesus David Posada Escobar <posesco@gmail.com>
2025-12-29 08:01:37 +0100

    Simplification of local env

--- projects/ec2_n8n/ec2.tf
+++ projects/ec2_n8n/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "lab_instance" {
   ami                         = data.aws_ami.os.id
-  instance_type               = var.instance_type[terraform.workspace]
+  instance_type               = var.instance_type[local.env]
   vpc_security_group_ids      = [aws_security_group.lab_sg.id]
-  key_name                    = var.key_name[terraform.workspace]
+  key_name                    = var.key_name[local.env]
   associate_public_ip_address = true
   user_data = templatefile("${path.module}/user_data.sh", {
-    cloudflare_tunnel_token = var.cloudflare_tunnel_token[terraform.workspace]
+    cloudflare_tunnel_token = var.cloudflare_tunnel_token[local.env]
   })
   subnet_id            = data.terraform_remote_state.networking.outputs.public_subnet_ids[0]
   iam_instance_profile = data.terraform_remote_state.iam.outputs.ec2_projects_instance_profile_name
@@ -17,7 +17,7 @@ resource "aws_instance" "lab_instance" {
 
   root_block_device {
     volume_type = "gp3"
-    volume_size = var.lab_volume_size[terraform.workspace]
+    volume_size = var.lab_volume_size[local.env]
     encrypted   = true
     tags = merge(
       local.common_tags,
